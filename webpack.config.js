05bf45e7ae6981f45660ee85fa68bfa45cbef5ac
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-08-17 19:14:02 -0500

    update indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const packageName = require('./package.json').name;
 
 module.exports = {
-  entry: './index',
-  output: {
-    path: __dirname + '/dist',
-    filename: `${packageName}.js`
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js/,
-        exclude: /node_modules/,
-        loader: 'babel'
-      }
-    ]
-  }
+    entry: './index',
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel',
+                test: /\.js/
+            }
+        ]
+    },
+    output: {
+        filename: `${packageName}.js`,
+        path: `${__dirname}/dist`
+    }
 };
