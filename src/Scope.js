7ae94ddafb3b3d5e8f149c5422ef5baecdfb1c08
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-08-17 19:19:59 -0500

    fire listener on first digest cycle

--- src/Scope.js
+++ src/Scope.js
@@ -1,3 +1,28 @@
+import {forEach} from 'lodash';
+
+const $$watchers = new WeakMap();
+
 export default class Scope {
+
+    constructor() {
+
+        $$watchers.set(this, []);
+
+    }
+
+    $watch(watchFn, listenerFn) {
+
+        $$watchers.get(this).push({
+            listenerFn,
+            watchFn
+        });
+
+    }
+
+    $digest() {
+
+        forEach($$watchers.get(this), watcher => watcher.listenerFn());
+
+    }
 
 }
--- test/Scope.js
+++ test/Scope.js
@@ -1,16 +1,48 @@
 import Scope from '../src/Scope';
 import {expect} from 'code';
+import sinon from 'sinon';
 
 describe('Scope', () => {
 
-  it('should be constructed and used as an object', () => {
+    let $scope,
+        listenerFn,
+        sandbox,
+        watchFn;
 
-    const scope = new Scope();
+    beforeEach(() => {
 
-    scope.aProperty = 1;
+        $scope = new Scope();
+        sandbox = sinon.sandbox.create();
+        listenerFn = sandbox.stub();
+        watchFn = sandbox.stub();
 
-    expect(scope.aProperty).equals(1);
+    });
 
-  });
+    afterEach(() => sandbox.restore());
+
+    it('should be constructed and used as an object', () => {
+
+        $scope.aProperty = 1;
+
+        expect($scope.aProperty).equals(1);
+
+    });
+
+    describe('digest', () => {
+
+        it('should call the listener function of a watch on first $digest', () => {
+
+            $scope.$watch(
+                () => 'watch',
+                listenerFn
+            );
+
+            $scope.$digest();
+
+            sinon.assert.calledOnce(listenerFn);
+
+        });
+
+    });
 
 });
