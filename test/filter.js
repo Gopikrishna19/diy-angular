95d0decc801d45d4edb16824d54d22fa067a6a2d
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-11-27 09:36:22 -0600

    filter based on a predicate function

---
+++ test/filter.js
@@ -0,0 +1,23 @@
+import {expect} from 'code';
+import {filter} from '../src/filters';
+import parse from '../src/parse';
+
+describe('Filter filter', () => {
+
+    it('should be available', () => {
+
+        expect(filter('filter')).function();
+
+    });
+
+    it('should filter based on a predicate', () => {
+
+        const $scope = {
+            isEven: n => n % 2 === 0
+        };
+
+        expect(parse('[0, 1, 2, 3] | filter : isEven')($scope)).equals([0, 2]);
+
+    });
+
+});
